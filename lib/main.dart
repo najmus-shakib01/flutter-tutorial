51793a8686216034634221c5f6cbe3f2700dd28d
najmus-shakib01 <syednajmusshakib130@gmail.com>
2025-12-09 18:15:03 +0600

    feat(simple_project): simple project sucessfully completed.

--- lib/main.dart
+++ lib/main.dart
@@ -4,13 +4,80 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+// ignore: non_constant_identifier_names
+String ButtonName = "Click";
+int currentIndex = 0;
 
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(backgroundColor: Colors.blue, title: Text("App Title")),
+        body: Center(
+          child: currentIndex == 0
+              ? Container(
+                  width: double.infinity,
+                  height: double.infinity,
+                  color: Colors.grey,
+
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.orange,
+                          foregroundColor: Colors.white,
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            ButtonName = "Clicked";
+                          });
+                        },
+                        child: Text(ButtonName),
+                      ),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.red,
+                          foregroundColor: Colors.white,
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            ButtonName = "Clicked";
+                          });
+                        },
+                        child: Text(ButtonName),
+                      ),
+                    ],
+                  ),
+                )
+              : Image.network('images/888-400x300.jpg'),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const [
+            BottomNavigationBarItem(label: "Home", icon: Icon(Icons.favorite)),
+            BottomNavigationBarItem(
+              label: "Settings",
+              icon: Icon(Icons.settings),
+            ),
+          ],
+          currentIndex: currentIndex,
+          onTap: (int index) {
+            setState(() {
+              currentIndex = index;
+            });
+          },
+        ),
+      ),
     );
   }
 }
